d7ce189aeaf57acdf8e0d194093508bd0cf2a0b9
tiagoworking5 <tiagoworking5@gmail.com>
2026-01-27 21:25:05 -0300

    exercicio_erros

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function veriricarSenhas() {
+function verificarSenha() {
 
-    let usuario = document.getElementById("usuario").value;
-    let senha = document.getElementById("inpsenha").value;
-    let mensagem = document.getElementById("pMensagem");
+    let usuario = document.getElementById('ddlUsuario').value;
+    let senha = document.getElementById('inpSenha').value;
+    let mensagem = document.getElementById('pMensagem');
 
     //se a dropdownlist não estiver selecionada
     if (usuario === "0") {
@@ -11,18 +11,18 @@ function veriricarSenhas() {
 
     // Usuário comum
     else if (usuario === "1" && senha === "123") {
-        alert();
-        window.location.href = "Bancaria.html";
+        alert("Você será direcionado para a página de conta cliente.");
+        window.location.href = "contaBancaria.html";
     }
 
     // Conta salário
     else if (usuario === "2" && senha === "456") {
-        alert("Você será direcionado para a página de conta saláraio.");
+        alert("Você será direcionado para a página de conta salário.");
         window.location.href = "contaSalario.html";
     }
 
     // Aposentado
-    else if (usuario === "Aposentado" && senha === "789") {
+    else if (usuario === "3" && senha === "789") {
         alert("Você será direcionado para a página de benefício aposentadoria.");
         window.location = "aposentado.html";
     }
